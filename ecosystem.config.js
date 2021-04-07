dfb75be4ba44e77af64aca333d277c69ce96766c
Eduard Albu <eduard.albu@gmail.com>
2021-04-08 02:45:45 +0300

    updated configurations

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,7 @@ module.exports = {
       name: 'easyplan-app',
       mode: 'cluster',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
-      env_development: {
-        PORT: 3001,
-        APP_ENV: 'development',
-      },
-      env_production: {
-        PORT: 3000,
-        APP_ENV: 'production',
-      },
+      args: 'start -p 3001',
     },
   ],
 };
