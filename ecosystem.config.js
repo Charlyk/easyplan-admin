7d5760daa15d49e4e7c6a835e9eb4b79de2a007a
Eduard Albu <eduard.albu@gmail.com>
2021-04-08 02:39:36 +0300

    updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,11 @@ module.exports = {
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env_development: {
+        PORT: 3001,
         APP_ENV: 'development',
       },
       env_production: {
+        PORT: 3000,
         APP_ENV: 'production',
       },
     },
