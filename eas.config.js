7b3f5376205a18d1c5475e78198492f5c88d1507
Eduard Albu <eduard.albu@gmail.com>
2021-04-10 23:54:19 +0300

    Updated api urls

--- eas.config.js
+++ eas.config.js
@@ -5,7 +5,7 @@ export const baseApiUrl =
   environment === 'local'
     ? 'http://{clinicDomain}api.easyplan.loc:8081/api'
     : environment === 'development'
-      ? 'https://dev-api.easyplan.pro/api'
-      : 'https://api.easyplan.pro/api';
+      ? 'https://{clinicDomain}dev-api.easyplan.pro/api'
+      : 'https://{clinicDomain}api.easyplan.pro/api';
 
 export const imageLambdaUrl = 'https://d25mcgbnpi.execute-api.eu-west-1.amazonaws.com/production';
