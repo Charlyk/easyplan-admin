c4dc7a49f95570cd3c8830692ad1fdae93bd3153
Eduard Albu <eduard.albu@gmail.com>
2021-04-11 12:55:13 +0300

    Redirecting user to clinics list from index page if user is on general domain.

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ const MainPage = ({ currentClinic, currentUser }) => {
   }, []);
 
   const redirectUserToPage = async () => {
+    const [subdomain] = window.location.host.split('.');
+    if (['app', 'app-dev'].includes(subdomain)) {
+      await router.replace('/clinics');
+      return;
+    }
     const redirectPath = getRedirectUrlForUser(currentUser);
     await router.replace(redirectPath);
   }
