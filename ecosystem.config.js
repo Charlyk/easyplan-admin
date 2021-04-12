c27a1fe059087a8312dbc2ffee2dbcb2c933f087
Eduard Albu <eduard.albu@gmail.com>
2021-04-12 19:04:22 +0300

    removed ecosystem.config.js from .gitignore

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: 'easyplan-dev',
+      mode: 'cluster',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3001',
+    },
+  ],
+};
