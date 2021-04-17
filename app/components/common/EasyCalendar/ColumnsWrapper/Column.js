0efa962fd56e071d6696a3f0d39ff887e6742990
Eduard Albu <eduard.albu@gmail.com>
2021-04-17 20:21:23 +0300

    Replaced moment with moment-timezone

--- app/components/common/EasyCalendar/ColumnsWrapper/Column.js
+++ app/components/common/EasyCalendar/ColumnsWrapper/Column.js
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import PropTypes from 'prop-types';
 import isEqual from 'lodash/isEqual';
 import orderBy from 'lodash/orderBy';
-import Moment from "moment";
+import Moment from "moment-timezone";
 import { extendMoment } from "moment-range";
 import { useSelector } from "react-redux";
 import {
