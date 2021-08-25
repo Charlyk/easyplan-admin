12ff9318002eacb484c4805525e6bd78a03f8232
Eduard Albu <eduard.albu@gmail.com>
2021-08-26 00:02:14 +0300

    Removed upload button from calendar header

--- app/components/dashboard/calendar/CalendarHeader/CalendarHeader.js
+++ app/components/dashboard/calendar/CalendarHeader/CalendarHeader.js
@@ -166,13 +166,6 @@ const CalendarHeader = (
         />
       </div>
       <div className={clsx(styles['right-btns-wrapper'], 'flexContainer')}>
-        <LoadingButton
-          variant='outline-primary'
-          className={clsx('btn-outline-primary', styles['import-btn'])}
-          onClick={onImportSchedules}
-        >
-          <UploadIcon/>
-        </LoadingButton>
         <ClickAwayListener onClickAway={handleCloseLegend}>
           <IconButton
             ref={legendAnchor}
