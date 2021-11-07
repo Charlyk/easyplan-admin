1da8b1af15b838c26047d8ece9c9ec6b7dabf9d7
Eduard Albu <eduard.albu@gmail.com>
2021-11-07 19:48:31 +0200

    Showing crm for dentus only in production

--- app/components/common/MainComponent/MainMenu/MainMenu.jsx
+++ app/components/common/MainComponent/MainMenu/MainMenu.jsx
@@ -37,6 +37,7 @@ import ClinicSelector from '../../ClinicSelector';
 import EASHelpView from "../../EASHelpView";
 import ExchangeRates from "../ExchageRates";
 import styles from './MainMenu.module.scss';
+import { environment } from "../../../../../eas.config";
 
 const menuItems = [
   {
@@ -157,6 +158,11 @@ const MainMenu = ({ currentPath, currentUser, currentClinic, onCreateClinic }) =
   const [isAnalyticsExpanded, setIsAnalyticsExpanded] = useState(
     currentPath.startsWith('/analytics'),
   );
+  const canShowCrm =
+    environment !== 'production' ||
+    currentClinic.id === 1772 ||
+    currentClinic.id === 46 ||
+    currentClinic.id === 289747;
 
   useEffect(() => {
     setIsAnalyticsExpanded(checkIsAnalyticsEnabled());
@@ -167,11 +173,14 @@ const MainMenu = ({ currentPath, currentUser, currentClinic, onCreateClinic }) =
   }, []);
 
   useEffect(() => {
+    if (!canShowCrm) {
+      return;
+    }
     setShowCrmHelp(
       !wasNotificationShown(notifications.menuCRMImplementation.id) &&
       !showTechSupportHelp,
     );
-  }, [showTechSupportHelp]);
+  }, [showTechSupportHelp, canShowCrm]);
 
   useEffect(() => {
     fetchRemindersCount();
@@ -315,6 +324,9 @@ const MainMenu = ({ currentPath, currentUser, currentClinic, onCreateClinic }) =
               </React.Fragment>
             )
           } else if (item.type === 'link') {
+            if (item.id === 'crm' && !canShowCrm) {
+              return null;
+            }
             return (
               <Link href={item.href} key={item.id}>
                 <ListItem
