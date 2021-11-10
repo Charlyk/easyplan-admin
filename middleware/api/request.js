d669779c5bf39505fa4afab12126956cb497ecd0
Eduard Albu <eduard.albu@gmail.com>
2021-11-10 22:31:56 +0200

    ignoring certificate on dev and local

--- middleware/api/request.js
+++ middleware/api/request.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { environment, isDev } from "../../eas.config";
+import { Agent } from "https";
 
 const host = environment === 'local' ? 'http://app.easyplan.loc' : 'http://localhost';
 const port = environment === 'local' ? 80 : isDev ? 3001 : 3000
@@ -13,23 +14,27 @@ const port = environment === 'local' ? 80 : isDev ? 3001 : 3000
  * @return {Promise<AxiosResponse<any>>}
  */
 export async function request(path, method, headers = null, body = null) {
+  const agent = new Agent({
+    rejectUnauthorized: environment !== 'production',
+  });
+
   switch (method) {
     case "get":
       return headers
-        ? axios.get(`${host}:${port}${path}`, { headers })
-        : axios.get(path);
+        ? axios.get(`${host}:${port}${path}`, { headers, httpsAgent: agent })
+        : axios.get(path, { httpsAgent: agent });
     case 'post':
       return headers
-        ? axios.post(`${host}:${port}${path}`, body, { headers })
-        : axios.post(path, body);
+        ? axios.post(`${host}:${port}${path}`, body, { headers, httpsAgent: agent })
+        : axios.post(path, body, { httpsAgent: agent });
     case 'put':
       return headers
-        ? axios.put(`${host}:${port}${path}`, body, { headers })
-        : axios.put(path, body);
+        ? axios.put(`${host}:${port}${path}`, body, { headers, httpsAgent: agent })
+        : axios.put(path, body, { httpsAgent: agent });
     case 'delete':
       return headers
-        ? axios.delete(`${host}:${port}${path}`, { headers })
-        : axios.delete(path);
+        ? axios.delete(`${host}:${port}${path}`, { headers, httpsAgent: agent })
+        : axios.delete(path, { httpsAgent: agent });
     default:
       throw Error('Method not allowed');
   }
