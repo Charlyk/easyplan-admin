ece2c1006d6b825b7258a2c6fde4a32b8ff6b0cc
Eduard Albu <eduard.albu@gmail.com>
2021-11-11 13:39:58 +0200

    Removed setting origin header cause it not working

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,13 +1,9 @@
 import moment from "moment-timezone";
 import Axios from "axios";
-import { environment } from "../../eas.config";
 
 Axios.interceptors.request.use(function (config) {
   config.headers['X-EasyPlan-TimeZone'] = moment.tz.guess(true);
   config.headers['Accept-Language'] = 'ro';
   config.headers['X-EasyPlan-Platform'] = 'web';
-  if (environment === 'testing') {
-    config.headers['Origin'] = '*';
-  }
   return config;
 });
