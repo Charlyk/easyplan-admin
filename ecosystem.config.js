5298751d949f56637ce04436f79d9ccab9b356c2
Eduard Albu <eduard.albu@gmail.com>
2021-11-12 13:31:50 +0200

    Fixed workflow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   apps: [{
     name: "easyplan-admin",
-    script: 'yarn',
+    script: 'node_modules/next/dist/bin/next',
     args: 'start',
+    cwd: './',
+    instances: 'max',
     exec_mode: 'cluster',
-    interpreter: '/bin/bash',
   }]
 }
