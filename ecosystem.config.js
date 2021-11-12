20734442de7335e6ec2a26db4f9b218eecda6623
Eduard Albu <eduard.albu@gmail.com>
2021-11-12 13:24:48 +0200

    Copy build directory for nginx use

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  apps : [{
-    name   : "easyplan-admin",
-    script    : 'yarn',
-    args      : 'start',
+  apps: [{
+    name: "easyplan-admin",
+    script: 'yarn',
+    args: 'start',
+    exec_mode: 'cluster',
     interpreter: '/bin/bash',
   }]
 }
