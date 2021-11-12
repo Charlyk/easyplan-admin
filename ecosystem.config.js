667feb53e4324589e60298d01928599744d7f437
Eduard Albu <eduard.albu@gmail.com>
2021-11-12 13:59:47 +0200

    fixed pm2 configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: "easyplan-admin",
-      script: 'node_modules/next/dist/bin/next',
+      script: 'yarn',
       args: 'start',
-      cwd: './',
       instances: 'max',
       exec_mode: 'cluster',
       interpreter: '/bin/bash',
