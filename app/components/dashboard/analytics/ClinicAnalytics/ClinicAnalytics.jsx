54f73ee62c8f40ee4d1de27bfd672ac785041016
Eduard Albu <eduard.albu@gmail.com>
2021-11-18 18:28:56 +0200

    [EAS-316][EAS-324] Created a new page to display clinic analytics

---
+++ app/components/dashboard/analytics/ClinicAnalytics/ClinicAnalytics.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './ClinicAnalytics.module.scss';
+
+const ClinicAnalytics = ({ currentUser, currentClinic }) => {
+  return (
+    <div className={styles.clinicAnalytics}>
+      <div/>
+    </div>
+  );
+};
+
+export default ClinicAnalytics;
