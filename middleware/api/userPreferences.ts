49918daa75350d56a316859278f7e3fefdbf43cc
=dadadaradan <vacaridan@yahoo.com>
2021-12-16 13:24:41 +0200

    integrated the ui with the backend API

--- middleware/api/userPreferences.ts
+++ middleware/api/userPreferences.ts
@@ -1,3 +1,23 @@
 import { put } from './request';
 
-// export async function  () {};
+export async function requestAllowDoctorCreateSchedules(
+  userId: number,
+  headers = null,
+) {
+  const body = {
+    canCreate: true,
+  };
+
+  return put(`/api/userPreferences/schedules/${userId}`, headers, body);
+}
+
+export async function requestForbidDoctorCreateSchedules(
+  userId: number,
+  headers = null,
+) {
+  const body = {
+    canCreate: false,
+  };
+
+  return put(`/api/userPreferences/schedules/${userId}`, headers, body);
+}
