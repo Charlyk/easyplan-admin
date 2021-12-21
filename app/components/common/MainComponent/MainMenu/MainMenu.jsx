fd2af7e40d02705048f6c423edca21a126b3c6af
Eduard Albu <eduard.albu@gmail.com>
2021-12-21 12:27:53 +0200

    Fixed switching clinics for dev and stage environments

--- app/components/common/MainComponent/MainMenu/MainMenu.jsx
+++ app/components/common/MainComponent/MainMenu/MainMenu.jsx
@@ -31,7 +31,7 @@ import { textForKey } from 'app/utils/localization';
 import notifications from 'app/utils/notifications/notifications';
 import updateNotificationState from 'app/utils/notifications/updateNotificationState';
 import wasNotificationShown from 'app/utils/notifications/wasNotificationShown';
-import { environment } from 'eas.config';
+import { appBaseUrl, environment } from 'eas.config';
 import {
   currentClinicSelector,
   currentUserSelector,
@@ -226,12 +226,9 @@ const MainMenu = ({ currentPath, onCreateClinic }) => {
   };
 
   const handleCompanySelected = async (company) => {
-    const [_, domain, location, suffix] = window.location.host.split('.');
-    const { protocol } = window.location;
-    const clinicUrl = `${protocol}//${
-      company.clinicDomain
-    }.${domain}.${location}${environment === 'testing' ? `.${suffix}` : ''}`;
-    window.open(clinicUrl, '_blank');
+    const { clinicDomain } = company;
+    const redirectUrl = appBaseUrl.replace('app', clinicDomain);
+    window.open(redirectUrl, '_blank');
     handleCompanyClose();
   };
 
