392758ebf1e027e522d1a1ded1eb8d04c00728c2
=dadadaradan <vacaridan@yahoo.com>
2022-01-03 19:26:38 +0200

    added types

--- redux/sagas/requests/calendar/requestUpdateScheduleDoctorAndDate.ts
+++ redux/sagas/requests/calendar/requestUpdateScheduleDoctorAndDate.ts
@@ -3,9 +3,15 @@ import Moment from 'moment-timezone';
 import { updateScheduleDoctorAndDate } from 'middleware/api/schedules';
 import { Schedule } from 'types';
 
-export async function requestUpdateScheduleDoctorAndDate(
-  props,
-): Promise<AxiosResponse<Schedule>> {
+export async function requestUpdateScheduleDoctorAndDate(props: {
+  schedule: Schedule;
+  body: {
+    doctorId: number;
+    startDate?: string;
+    cabinetId?: number;
+    doctorServices?: any[];
+  };
+}): Promise<AxiosResponse<Schedule>> {
   const { schedule, body } = props;
   const startDate = Moment(body.startDate).toDate();
 
