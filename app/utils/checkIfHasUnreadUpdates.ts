f764bb6364b89eb20ca4215c1927523d84cc7764
Eduard Albu <eduard.albu@gmail.com>
2022-01-13 18:09:15 +0200

    Some changes to ChangeLogModal styles and logic

--- app/utils/checkIfHasUnreadUpdates.ts
+++ app/utils/checkIfHasUnreadUpdates.ts
@@ -1,6 +1,10 @@
 import { checkForNewRecentChanges } from 'middleware/api/recentChanges';
 
 export const checkIfHasUnreadUpdates = async () => {
-  const response = await checkForNewRecentChanges();
-  return response.data;
+  try {
+    const response = await checkForNewRecentChanges();
+    return response.data;
+  } catch (error) {
+    console.warn(error);
+  }
 };
