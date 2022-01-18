22cd3ccfb5d8f8bdbb9be07661e8d9a3dbcc0870
Eduard Albu <eduard.albu@gmail.com>
2022-01-18 22:35:01 +0200

    [EAS-542] Fixed build warnings about import order

--- redux/slices/calendarData.ts
+++ redux/slices/calendarData.ts
@@ -5,7 +5,7 @@ import { HYDRATE } from 'next-redux-wrapper';
 import initialState from 'redux/initialState';
 import { CalendarDataState } from 'redux/types';
 import { ScheduleItem, ScheduleDetails, Schedule } from 'types';
-import { FilterData } from '../types/types';
+import { FilterData } from '../types';
 
 const isScheduleInGroup = (group: ScheduleItem, schedule: Schedule) => {
   const scheduleDate = moment(schedule.startTime).format('YYYY-MM-DD');
