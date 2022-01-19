b7d7c7d22f7d154075fac10c727f3a4c6ab9a6d1
=dadadaradan <vacaridan@yahoo.com>
2022-01-19 15:50:31 +0200

    fixed import order

--- app/components/dashboard/patients/PatientDetailsModal/PatientNotes/PatientNote/PatientNote.jsx
+++ app/components/dashboard/patients/PatientDetailsModal/PatientNotes/PatientNote/PatientNote.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 import moment from 'moment-timezone';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
 import { textForKey } from 'app/utils/localization';
 import styles from './PatientNote.module.scss';
 
