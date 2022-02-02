dd5687bacbce6c4b81984431614b4ecd7dc4ab0f
Eduard Albu <eduard.albu@gmail.com>
2022-02-02 23:32:12 +0200

    Removed unused imports

--- app/components/crm/DealsColumn/DealItem/DealItem.tsx
+++ app/components/crm/DealsColumn/DealItem/DealItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useDrag } from 'react-dnd';
-import { useSelector } from 'react-redux';
-import { currentClinicSelector } from 'redux/selectors/appDataSelector';
 import { CrmDealListItemType, DealStateType } from 'types';
 import { ItemTypes } from '../constants';
 import SortedDealItem from '../SortedDealItem';
