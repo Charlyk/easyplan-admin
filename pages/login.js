faa3a9df71c3ad9e3770125e9cbef476b6c309b6
Eduard Albu <eduard.albu@gmail.com>
2022-03-20 17:57:57 +0200

    redirecting to authentication app from login and registration page

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LoginWrapper from 'app/components/common/LoginWrapper';
-import checkIsMobileDevice from 'app/utils/checkIsMobileDevice';
-import getRedirectUrlForUser from 'app/utils/getRedirectUrlForUser';
-import parseCookies from 'app/utils/parseCookies';
-import setCookies from 'app/utils/setCookies';
-import { getCurrentUser } from 'middleware/api/auth';
-import { getClinicDetails } from 'middleware/api/clinic';
+import { loginUrl } from '../eas.config';
 
 const Login = ({ currentUser, currentClinic, authToken, isMobile }) => {
   return (
@@ -21,44 +16,11 @@ const Login = ({ currentUser, currentClinic, authToken, isMobile }) => {
 
 export default connect((state) => state)(Login);
 
-export const getServerSideProps = async ({ req, res }) => {
-  const isMobile = checkIsMobileDevice(req);
-  const { auth_token: authToken } = parseCookies(req);
-
-  if (authToken == null) {
-    return { props: { isMobile } };
-  }
-
-  const props = { authToken: authToken ?? null, isMobile };
-  try {
-    const response = await getCurrentUser(req.headers);
-    props.currentUser = response.data;
-  } catch (error) {
-    console.error('Login - user', error.message);
-  }
-
-  try {
-    const response = await getClinicDetails(null, req.headers);
-    props.currentClinic = response.data;
-    setCookies(res, authToken, props.currentClinic.id);
-  } catch (error) {
-    console.error('Login - clinic', error.message);
-  }
-
-  if (props.currentClinic != null && props.currentUser != null) {
-    const redirectTo = getRedirectUrlForUser(
-      props.currentUser,
-      props.currentClinic.domainName,
-    );
-    if (redirectTo != null) {
-      return {
-        redirect: {
-          destination: redirectTo,
-          permanent: false,
-        },
-      };
-    }
-  }
-
-  return { props };
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: loginUrl,
+      permanent: true,
+    },
+  };
 };
--- pages/register.js
+++ pages/register.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { END } from 'redux-saga';
 import RegistrationWrapper from 'app/components/common/RegistrationWrapper';
-import checkIsMobileDevice from 'app/utils/checkIsMobileDevice';
-import { setCookies } from 'redux/slices/appDataSlice';
 import { wrapper } from 'store';
+import { registerUrl } from '../eas.config';
 
 const Register = ({ isMobile }) => {
   return <RegistrationWrapper isMobile={isMobile} />;
@@ -13,21 +12,16 @@ const Register = ({ isMobile }) => {
 export default connect((state) => state)(Register);
 
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) =>
-    async ({ req }) => {
-      // end the saga
-      store.dispatch(END);
-      await store.sagaTask.toPromise();
+  (store) => async () => {
+    // end the saga
+    store.dispatch(END);
+    await store.sagaTask.toPromise();
 
-      // fetch page data
-      const isMobile = checkIsMobileDevice(req);
-      const cookies = req?.headers?.cookie ?? '';
-      store.dispatch(setCookies(cookies));
-
-      return {
-        props: {
-          isMobile,
-        },
-      };
-    },
+    return {
+      redirect: {
+        destination: registerUrl,
+        permanent: true,
+      },
+    };
+  },
 );
