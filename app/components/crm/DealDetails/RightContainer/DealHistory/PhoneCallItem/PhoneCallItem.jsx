c48dd315f0a1e2a7aa84388df1b994ce48a0a48a
Eduard Albu <eduard.albu@gmail.com>
2022-06-22 22:48:20 +0300

    Fixed error for phone calls

--- app/components/crm/DealDetails/RightContainer/DealHistory/PhoneCallItem/PhoneCallItem.jsx
+++ app/components/crm/DealDetails/RightContainer/DealHistory/PhoneCallItem/PhoneCallItem.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import PhoneIcon from '@material-ui/icons/Call';
